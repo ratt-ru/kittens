5489bcf44e7ac69e3ced4a51f537df51988141e5
Gijs Molenaar <gijs@pythonic.nl>
2014-01-24 11:40:24 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kittens',
+      version='1.3.0',
+      description='',
+      author='Oleg Smirnov',
+      author_email='Oleg Smirnov <osmirnov@gmail.com>',
+      url='https://github.com/ska-sa/kittens',
+      packages=['Kittens'],
+     )
