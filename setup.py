3b4afc4d621bc40bfbd7f18343971e434c41af2a
Gijs Molenaar <gijs@pythonic.nl>
2014-01-24 11:58:55 +0200

    update description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='kittens',
       version='1.3.0',
-      description='',
+      description='Collection of Python utility functions for purr, tigger, meqtrees and others',
       author='Oleg Smirnov',
       author_email='Oleg Smirnov <osmirnov@gmail.com>',
       url='https://github.com/ska-sa/kittens',
