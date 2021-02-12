12da9fd4b537a92f16c9d8178d2ef7ff52fd08e6
Benjamin Hugo <bennahugo@gmail.com>
2021-02-12 09:38:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from Kittens import __version__
 
 setup(name='astro-kittens',
       version=__version__,
+      python_requires='>=3.0.0',
       description='Collection of Python utility functions for purr, tigger, meqtrees and others',
       author='Oleg Smirnov',
       author_email='Oleg Smirnov <osmirnov@gmail.com>',
